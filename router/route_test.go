a2db3850da91b6f5f459bdcae52be781dfb07c77
Ke Zhu <kzhu@us.ibm.com>
2016-12-05 11:42:48 -0500

    make sure JWT token works

--- router/route_test.go
+++ router/route_test.go
@@ -3,13 +3,28 @@ package router
 import (
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 
 	. "github.com/franela/goblin"
 )
 
 func createTestApp() http.Handler {
 	return Routes()
+}
+
+func generateJWTToken() (string, error) {
+	jwtSecret := []byte(os.Getenv("JWT_SECRET"))
+	jwtClaims := jwt.StandardClaims{
+		ExpiresAt: time.Now().Add(1 * time.Hour).Unix(),
+		Issuer:    "test",
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwtClaims)
+	return token.SignedString(jwtSecret)
 }
 
 func TestJWT(t *testing.T) {
@@ -24,6 +39,16 @@ func TestJWT(t *testing.T) {
 			app.ServeHTTP(resp, req)
 
 			g.Assert(resp.Code).Equal(http.StatusUnauthorized)
+		})
+
+		g.It("supports JWT token", func() {
+			jwtToken, _ := generateJWTToken()
+			req, _ := http.NewRequest("GET", "/builds/foo", nil)
+			req.Header.Set("Authorization", "BEARER "+jwtToken)
+			resp := httptest.NewRecorder()
+			app.ServeHTTP(resp, req)
+
+			g.Assert(resp.Code).Equal(http.StatusOK)
 		})
 	})
 }
